50b03b77e9a0694586384643ea7809e25c0b7410
selimttasdemir <selimtasdemir.2022@gmail.com>
2025-11-05 15:12:59 +0300

    update

--- backend/server.py
+++ backend/server.py
@@ -1350,7 +1350,10 @@ app.include_router(api_router)
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://ibca-frontend.onrender.com",
+    ],
     allow_methods=["*"],
     allow_headers=["*"],
 )
